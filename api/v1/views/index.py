7a4669dcca0a34c35961c7f27c91a65001fd0d67
abybells <abbiolbello@yahoo.com>
2022-10-21 13:24:41 +0100

    api

---
+++ api/v1/views/index.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""index.py to connect to API"""
+from app.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
+from models import storage
+
+all_classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+        }
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """display status response"""
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """display number of objects by type"""
+    return_dict = {}
+    for key, value in all_classes.item():
+        return_dict[key] = storage.count(value)
+        return jsonify(return_dict)
+
+if __name__ = "__main__":
+    pass
