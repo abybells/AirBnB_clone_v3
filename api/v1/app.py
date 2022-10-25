4c62205a960defce5d24366cc592054a23d08005
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-25 13:32:36 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
 """app.py to connect to API"""
-from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify, make_response
-from flask_cors import CORS
+from flask import Flask, Blueprint, jsonify, make_response, render_template
 from models import storage
+from api.v1.views import app_views
+from os import environ
+from flask_cors import CORS
 import os
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views, url_prefix="/api/v1")
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_appcontext(code):
+def teardown_storage(x):
     """teardown_appcontext"""
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
+def error_handler(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(os.getenv('HBNB_API_PORT', '5000')), threaded=True)
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
