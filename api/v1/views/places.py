4da38999f15c22b082ba34109359a7731cb9b8ce
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-25 13:43:42 +0100

    Update places.py

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """views for places"""
 
-from app.v1.views import app_views
+from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from models import storage
 from models.city import City
